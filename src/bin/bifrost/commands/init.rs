ca136c13ce3414b7c22bae2c38129d782bf5112e
ericdeansanchez <ericdeansanchez@berkeley.edu>
2019-05-21 10:52:35 -0700

    impls common interface

--- src/bin/bifrost/commands/init.rs
+++ src/bin/bifrost/commands/init.rs
@@ -1,6 +1,11 @@
 use bifrost::ops::bifrost_init;
 use bifrost::util::BifrostResult;
+use bifrost::core::config::Config;
 
-pub fn exec() -> BifrostResult<()> {
+extern crate clap;
+use clap::ArgMatches;
+
+/// [NEED] parse args, update config if necessary...
+pub fn exec(config: Config, args: &ArgMatches) -> BifrostResult<()> {
     return bifrost_init::init();
 }
--- src/bin/bifrost/main.rs
+++ src/bin/bifrost/main.rs
@@ -7,15 +7,12 @@ pub mod commands;
 fn main() -> BifrostResult<()> {
     // Open the bifrost.
     let app = bifrost::cli().get_matches();
-    // config then,
-    // ...,
     let config = Config::default();
-    let workspace = WorkSpace::init(config);
 
     match app.subcommand() {
-        ("init", Some(init_matches)) => {
-            commands::init::exec()?;
-        }
+        ("init", Some(arg_matches)) => {
+            commands::init::exec(config, arg_matches)?;
+        },
         _ => {
             println!("{}", bifrost::util::template::EXPLICIT_LONG_HELP);
         }
