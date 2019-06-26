0a8e00d4738da0c31d97b7fc3a110229795bc487
ericdeansanchez <ericdeansanchez@berkeley.edu>
2019-06-26 15:50:32 -0700

    fixes hash before version

--- pkg/brew/bifrost-rs.rb
+++ pkg/brew/bifrost-rs.rb
@@ -4,7 +4,7 @@ class BifrostBin < Formula
     homepage "https://github.com/ericdeansanchez/bifrost"
   
     if OS.mac?
-        url "https://github.com/ericdeansanchez/bifrost/releases/download/v0.1.0-beta/bifrost-{version}-x86_64-apple-darwin18.6.0.tar.gz"
+        url "https://github.com/ericdeansanchez/bifrost/releases/download/v0.1.0-beta/bifrost-#{version}-x86_64-apple-darwin18.6.0.tar.gz"
         sha256 "d889de9db31b25e72b72dadf6064c0f54c7aad04fc910efd3fed6cc9451fe17a"
     end
 
